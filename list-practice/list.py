d16affefe5686883a3abf2edbcfd11216ca51cef
root <root@localhost.localdomain>
2019-11-26 11:42:44 -0500

    list practice

---
+++ list-practice/list.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+#Create a list
+#Have a menu add, remove number of items
+#Print the list after each operation
+
+import os
+os.system('clear')
+
+mylist = [1, 2, 3]
+
+while(True):
+    print('1 - Print list')
+    print('2 - Append to the list')
+    print('3 - Print the size of the list')
+    print('0 - Exit')
+    
+    option = int(input('Enter your choice: '))
+    print('\n')
+
+    #Print the list
+    if option == 1:
+        print(mylist)
+    
+    #Append new item to the end of the list
+    if option == 2:
+        item = input('Enter a new list item: ')
+        if item.isnumeric() == False:
+            mylist.append(item)
+        elif item.isnumeric() == True:
+            mylist.append(int(item))
+
+    #Print the number of items in the list
+    if option == 3:
+        print('The number of items in the list is: ', len(mylist))
+
+    #Exit
+    if option == 0:
+        print('Exiting...')
+        break
+
