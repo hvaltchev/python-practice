572f0b5413c88ea6e136e2addae27f503a6cb80f
Hristo Valtchev <hristo@valtchev.com>
2019-12-11 14:53:27 -0500

    game update

--- number-game/game.py
+++ number-game/game.py
@@ -4,17 +4,31 @@
 #Have the user guess numbers until its right
 #Say higher or lower
 #Keep track of nymber of trys
+#TODO Store player scores in a file 
+
 
 import random
+import sys
 
-number = random.randint(1,100)
+numrange = int(sys.argv[1])
+
+number = random.randint(1,numrange)
+attempts = 1
+
+print('The random number is in the range from 1 to', numrange)
 
 while (True):
     guess = int(input('Enter your guess: '))
+    attempts = attempts + 1
     if guess < number:
         print('Your guess is smaller.')
     elif guess > number:
         print('Your guess is bigger.')
-    elif guess == number:
+    else: 
         break
 print('You are correct.')
+print('You guessed in', attempts, 'attempts.')
+
+
+
+
